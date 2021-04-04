dd72c6387379c10b36e6d07a64dc609e02b7e572
Jon Moroney <darakian@gmail.com>
2021-04-04 12:59:30 -0700

    Begin real testing. Errors will be used as a source of plaintext going forward

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Token{
     Plaintext(String),
     Header(u8),
@@ -21,100 +21,16 @@ pub enum Token{
 }
 
 #[derive(Debug)]
-pub struct ParseError{
-    content: String,
+pub(crate) struct ParseError{
+    pub content: String,
 }
 
-pub fn lex(source: &str) -> (){
-    let mut char_iter = source.chars().peekable();
-    let mut tokens = Vec::new();
-    while char_iter.peek().is_some(){
-        // println!("Working {:?}", char_iter.peek());
-        match char_iter.peek(){
-            None => {return},
-            Some('#') => {
-                let token = lex_heading(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e),
-                }
-            },
-            Some('*') | Some('_') => {
-                let token = lex_asterisk_underscore(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e),
-                }
-            },
-            Some('-') | Some('+') => {
-                let token = lex_plus_minus(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e),
-                }
-            },
-            Some(' ') => {
-                let token = lex_spaces(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e)
-                }
-            },
-            Some('`') => {
-                let token = lex_backticks(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e)
-                }
-            },
-            Some('\n') => {
-                let token = lex_newlines(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e)
-                }
-            },
-            Some('>') => {
-                let token = lex_blockquotes(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e)
-                }
-            },
-            Some('!') => {
-                let token = lex_images(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e)
-                }
-            },
-            Some('[') => {
-                let token = lex_links(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e)
-                }
-            },
-            Some('<') => {
-                let token = lex_easy_links(&mut char_iter);
-                match token {
-                    Ok(t) => tokens.push(t),
-                    Err(e) => println!("{:?}", e)
-                }
-            },
-            Some(_) => {
-                let c = char_iter.next().unwrap();
-                push_char(&mut tokens, c);
-            },
-        }
-    }
-}
-
-pub fn push_char(t: &mut Vec<Token>, c: char) {
+pub(crate) fn push_char(t: &mut Vec<Token>, c: char) {
+    println!("pushing char: {}\nPrior token: {:?}", c, t.last());
     match t.last_mut() {
         Some(markdown_token) => {
             match markdown_token {
-                Token::Plaintext(mp) => mp.push(c),
+                Token::Plaintext(plaintext_token) => plaintext_token.push(c),
                 _ => t.push(Token::Plaintext(c.to_string())),
             }
         }
@@ -123,7 +39,7 @@ pub fn push_char(t: &mut Vec<Token>, c: char) {
 }
 
 use std::cmp;
-pub fn lex_heading(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_heading(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     let mut hashes = String::new();
     while char_iter.peek() == Some(&'#'){
         hashes.push(char_iter.next().unwrap());
@@ -137,7 +53,7 @@ pub fn lex_heading(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Resu
     }
 }
 
-pub fn lex_asterisk_underscore(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_asterisk_underscore(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     let mut asterunds = String::new();
     if char_iter.peek() == Some(&'*') {
         asterunds.push(char_iter.next().unwrap());
@@ -166,11 +82,11 @@ pub fn lex_asterisk_underscore(char_iter: &mut std::iter::Peekable<std::str::Cha
     }
 }
 
-pub fn lex_spaces(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError>{
+pub(crate) fn lex_spaces(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError>{
     let mut spaces = char_iter.next().unwrap().to_string();
     // Case 1: space in text => return char
     if char_iter.peek() != Some(&' ') {
-        return Ok(Token::Plaintext(spaces));
+        return Err(ParseError{content: spaces})
     }
     // Glob spaces
     while char_iter.peek() == Some(&' '){
@@ -193,7 +109,7 @@ pub fn lex_spaces(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Resul
     Err(ParseError{content: spaces})
 }
 
-pub fn lex_backticks(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_backticks(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     let mut ticks = char_iter.next().unwrap().to_string();
     match char_iter.peek(){
         Some(&'`') => {
@@ -231,7 +147,7 @@ pub fn lex_backticks(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Re
     }
 }
 
-pub fn lex_newlines(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_newlines(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     let mut new_lines = char_iter.next().unwrap().to_string();
     if char_iter.peek() != Some(&'\n') {
         return Ok(Token::InlineNewline);
@@ -242,7 +158,7 @@ pub fn lex_newlines(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Res
     Ok(Token::ParagraphBreak)
 }
 
-pub fn lex_blockquotes(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_blockquotes(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     let mut level = 0;
     while char_iter.next_if_eq(&'>').is_some() {
         level+=1;
@@ -250,7 +166,7 @@ pub fn lex_blockquotes(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> 
     Ok(Token::BlockQuote(level))
 }
 
-pub fn lex_images(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_images(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     let bang = char_iter.next().unwrap();
     match char_iter.peek(){
         Some(&'[') => {
@@ -307,7 +223,7 @@ pub fn lex_images(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Resul
     }
 }
 
-pub fn lex_links(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_links(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     match char_iter.peek(){
         Some(&'[') => {
             char_iter.next();
@@ -380,7 +296,7 @@ pub fn lex_links(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result
     }
 }
 
-pub fn lex_easy_links(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_easy_links(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     match char_iter.peek() {
         Some(&'<') => {
             let mut s = String::new();
@@ -403,7 +319,7 @@ pub fn lex_easy_links(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> R
     }
 }
 
-pub fn lex_plus_minus(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
+pub(crate) fn lex_plus_minus(char_iter: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, ParseError> {
     let c = char_iter.next().unwrap();
     if char_iter.next_if_eq(&' ').is_some(){
         return Ok(Token::UnorderedListEntry)
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod lexer;
-use crate::lexer::lex;
+use crate::lexer::*;
 
 #[cfg(test)]
 mod tests {
@@ -7,29 +7,127 @@ mod tests {
     
     #[test]
     fn test_lex() {
-        let heading_tests = vec![
-            ("# Heading level 1", "<h1>Heading level 1</h1>"),
-            ("## Heading level 2", "<h2>Heading level 2</h2>"),
-            ("### Heading level 3", "<h3>Heading level 3</h3>"),
-            ("#### Heading level 4", "<h4>Heading level 4</h4>"),
-            ("##### Heading level 5", "<h5>Heading level 5</h5>"),
-            ("###### Heading level 6", "<h6>Heading level 6</h6>"),
-            ("####### Invalid Heading level 7", "<h6>Heading level 6</h6>"),
-        ];
+        // let heading_tests = vec![
+        //     ("# Heading level 1", vec![Token::Header(1), ]),
+        //     ("## Heading level 2", "<h2>Heading level 2</h2>"),
+        //     ("### Heading level 3", "<h3>Heading level 3</h3>"),
+        //     ("#### Heading level 4", "<h4>Heading level 4</h4>"),
+        //     ("##### Heading level 5", "<h5>Heading level 5</h5>"),
+        //     ("###### Heading level 6", "<h6>Heading level 6</h6>"),
+        //     ("####### Invalid Heading level 7", "<h6>Heading level 6</h6>"), 
+        // 1
+        // ];
+        let mut heading_tests = Vec::new();
+        heading_tests.push(
+            ("# Heading level 1", 
+            vec![Token::Header(1),Token::Plaintext("Heading level 1".to_string()),]
+            )
+        );
         for test in heading_tests.iter(){
-            println!("Testing: {} -> {}", test.0, test.1);
-            lex(test.0);
+            println!("Testing: {} -> {:?}", test.0, test.1);
+            let tokens = lex(test.0);
+            assert_eq!(&tokens[..], &test.1[..]);
+            for token in tokens{
+                println!("Token: {:?}", token)
+            }
         }
 
-        let bold_tests = vec![
-            ("I just love **bold text**.", "I just love <strong>bold text</strong>."),
-            ("I just love __bold text__.", "I just love <strong>bold text</strong>."),
-            ("I just love *_bold text*_.", "I just love <strong>bold text</strong>."),
-            ("I just love\n\n\n _*bold text_*.", "I just love <strong>bold text</strong>."),
-        ];
-        for test in bold_tests.iter(){
-            // println!("Testing bold: {} -> {}", test.0, test.1);
-            lex(test.0);
+        // let bold_tests = vec![
+        //     ("I just love **bold text**.", "I just love <strong>bold text</strong>."),
+        //     ("I just love __bold text__.", "I just love <strong>bold text</strong>."),
+        //     ("I just love *_bold text*_.", "I just love <strong>bold text</strong>."),
+        //     ("I just love\n\n\n _*bold text_*.", "I just love <strong>bold text</strong>."),
+        // ];
+        // for test in bold_tests.iter(){
+        //     println!("Testing bold: {} -> {}", test.0, test.1);
+        //     lex(test.0);
+        // }
+    }
+}
+
+pub fn lex(source: &str) -> Vec<Token>{
+    let mut char_iter = source.chars().peekable();
+    let mut tokens = Vec::new();
+    while char_iter.peek().is_some(){
+        match char_iter.peek(){
+            None => {return tokens},
+            Some('#') => {
+                let token = lex_heading(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e),
+                }
+            },
+            Some('*') | Some('_') => {
+                let token = lex_asterisk_underscore(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e),
+                }
+            },
+            Some('-') | Some('+') => {
+                let token = lex_plus_minus(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e),
+                }
+            },
+            Some(' ') => {
+                let token = lex_spaces(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => {
+                        for c in e.content.chars(){push_char(&mut tokens, c)}
+                        println!("{:?}", e)}
+                }
+            },
+            Some('`') => {
+                let token = lex_backticks(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e)
+                }
+            },
+            Some('\n') => {
+                let token = lex_newlines(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e)
+                }
+            },
+            Some('>') => {
+                let token = lex_blockquotes(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e)
+                }
+            },
+            Some('!') => {
+                let token = lex_images(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e)
+                }
+            },
+            Some('[') => {
+                let token = lex_links(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e)
+                }
+            },
+            Some('<') => {
+                let token = lex_easy_links(&mut char_iter);
+                match token {
+                    Ok(t) => tokens.push(t),
+                    Err(e) => println!("{:?}", e)
+                }
+            },
+            Some(_) => {
+                let c = char_iter.next().unwrap();
+                push_char(&mut tokens, c);
+            },
         }
     }
+    tokens
 }
