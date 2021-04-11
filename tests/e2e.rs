10b42a74cf810988a3bb7d61e06b6b50173761a0
Jon Moroney <darakian@gmail.com>
2021-04-11 11:00:34 -0700

    and render tests

---
+++ tests/e2e.rs
@@ -0,0 +1,20 @@
+use mini_markdown::render;
+
+#[test]
+fn test_markdown_render() {
+    let mut tests = Vec::new();
+    tests.extend(vec![
+        ("# Heading level 1", "<h1>Heading level 1</h1>"),
+        ("## Heading level 2", "<h2>Heading level 2</h2>"),
+        ("### Heading level 3", "<h3>Heading level 3</h3>"),
+        ("#### Heading level 4", "<h4>Heading level 4</h4>"),
+        ("##### Heading level 5", "<h5>Heading level 5</h5>"),
+        ("###### Heading level 6", "<h6>Heading level 6</h6>"),
+        ("####### Invalid Heading level 7", "<h6>Heading level 1</h6>"), 
+    ]);
+
+    for test in tests.iter(){
+        let html = render(test.0);
+        assert_eq!(html, test.1);
+    }
+}
