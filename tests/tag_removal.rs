4a2030cfa9a308f0922147b050efee01a9fc1a2f
Jon Moroney <darakian@gmail.com>
2021-05-06 19:03:47 -0700

    A few more tests

--- tests/tag_removal.rs
+++ tests/tag_removal.rs
@@ -5,6 +5,8 @@ fn test_simple_tag_removal() {
     let mut tests = Vec::new();
     tests.extend(vec![
         ("<p>foobar <script src=123.com> text for context </script> </p> <junk>".to_string(), "foobar  text for context   ".to_string()),
+        ("Regular text with no tags but maybe a \n or a \t or something".to_string(), "Regular text with no tags but maybe a \n or a \t or something".to_string()),
+        ("And one more for the <p> money to test <!--- comment> unbalanced tags <a>".to_string(), "And one more for the  money to test  unbalanced tags ".to_string()),
     ]);
 
     for test in tests.iter_mut(){
