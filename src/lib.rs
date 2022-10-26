d42eaade744f2afa4f30175630f6e1abb9fd7738
Jon Moroney <darakian@gmail.com>
2022-10-25 21:06:38 -0700

    Working on example 4

--- src/lib.rs
+++ src/lib.rs
@@ -300,16 +300,16 @@ pub fn parse(tokens: &[Token]) -> String {
             Token::UnorderedListEntry(t) => {
                 if in_unordered_list == false {
                     in_unordered_list = true;
-                    html.push_str("<ul>")
+                    html.push_str("<ul>\n")
                 }
-                html.push_str(format!("<li>{}</li>", sanitize_display_text(t)).as_str())
+                html.push_str(format!("<li>\n{}</li>\n", sanitize_display_text(t)).as_str())
             },
             Token::OrderedListEntry(t) => {
                 if in_ordered_list == false {
                     in_ordered_list = true;
-                    html.push_str(format!("<ol>").as_str())
+                    html.push_str(format!("<ol>\n").as_str())
                 }
-                html.push_str(format!("<li>{}</li>", sanitize_display_text(t)).as_str())
+                html.push_str(format!("<li>\n{}</li>\n", sanitize_display_text(t)).as_str())
             },
             Token::Newline => {},
             Token::Tab => {html.push('\t')},
